d2c261afb8cae93fe797ba942c4f5d53eb4c21a4
Sanyam96 <sanyam.bvcoe96@gmail.com>
2018-11-02 23:31:36 +0530

    Register Seller

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  password: string
+  email: string
+  signupForm: FormGroup
+  registerError : string
+  registeredId : number
+
+  constructor(private formBuilder: FormBuilder,  private userService: UserService, private router: Router) {
+    this.signupForm = this.formBuilder.group({
+      ownerName: ['', Validators.compose([Validators.required])],
+      emailAddress: ['', Validators.compose([Validators.required])],
+      companyName: ['', Validators.compose([Validators.required])],
+      telephoneNumber: ['', Validators.compose([Validators.required])],
+      gstNumber: ['', Validators.compose([Validators.required])],
+      address: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  registerUser() {
+    if(this.signupForm.valid) {
+      console.log(this.signupForm.value);
+      
+      this.userService.registerUser(this.signupForm.value).subscribe((response : any) => {
+        this.registeredId = response.data.id
+        console.log(response.data.id)
+      },(error) => {
+        this.registerError = error
+      })
+    }
   }
 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -10,7 +10,9 @@ export class UserService {
 
   url: string = "http://localhost:8080/api/v1"
   currentUsername = new Subject<string>()
+
   constructor(private http: HttpClient) { }
+  
   loginUser(user: any) {
     const httpOptions = {
       headers: new HttpHeaders({
@@ -19,7 +21,28 @@ export class UserService {
     };
     return this.http.post(this.url+"/seller/login",user)
   }
-   setUsername(username: string) {
+
+  setUsername(username: string) {
     this.currentUsername.next(username)
+  }
+
+  registerUser(user:any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    }
+    const newUser = {
+      // seller : {
+        password : user.password,
+        companyName : user.companyName,
+        ownerName : user.ownerName,
+        emailAddress : user.emailAddress,
+        telephoneNumber : user.telephoneNumber,
+        gstNumber : user.gstNumber,
+        address : user.address    
+      // }  
+    }
+    return this.http.post(this.url+"/seller",newUser)
   }
 }
